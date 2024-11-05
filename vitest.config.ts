2438accb1633a7c1336ef1befb975a94bbb972ae
Laslo89 <matze_lebt@gmx.de>
2024-11-05 16:55:47 +0100

    config testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,12 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      globals: true,
       environment: 'jsdom',
+      setupFiles: './vitest.setup.ts', // Lädt die Setup-Datei für globale Mocks
+      coverage: {
+        reporter: ['text', 'json', 'html'], // Optional: Coverage-Reporter einrichten
+      },
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
